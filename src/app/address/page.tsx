6e64d9a96d0a28c46b29543598416952e90cd3fb
M-ali-raza346 <maliraza.346346@gmail.com>
2024-09-04 15:12:27 +0500

    next

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -3,20 +3,20 @@ import Image from 'next/image';
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/download.jpeg" 
-    alt="this is an image" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/address.png"
+     alt="this is am image" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-          “Our home provides us with a sense of protection and shelter from the outside world. We can be ourselves and let our guard down in the comfort of our homes. The feeling of safety and security that home provides is essential to our mental and emotional well-being.”
+          “MY address.”
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Dream
+          chowk pathanistan
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          house
+          Rahimyarkhan  pakistan
         </div>
       </figcaption>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,20 +3,28 @@ import Image from 'next/image';
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/download.jpeg" 
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/contact.jpeg" 
     alt="this is an image" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
+      <p className="text-lg font-medium text-red-500">
+        My name is mali raza
+
+      </p>
         <p className="text-lg font-medium">
-          “Our home provides us with a sense of protection and shelter from the outside world. We can be ourselves and let our guard down in the comfort of our homes. The feeling of safety and security that home provides is essential to our mental and emotional well-being.”
+          “this is my contact number.”
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          Dream
+          0302 8770999
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          house
+          this is my gmail contact
+        </div>
+        <div className="text-sky-500 dark:text-sky-400">
+        maliraza.346346 
+
         </div>
       </figcaption>
     </div>
